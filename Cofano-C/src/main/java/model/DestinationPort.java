ee057c16bf42c1a2bc2306e96968f50ebb84d510
Roberrt <banu_roby@yahoo.com>
2018-05-24 09:56:55 +0200

    Finished Model

--- Cofano-C/src/main/java/model/DestinationPort.java
+++ Cofano-C/src/main/java/model/DestinationPort.java
@@ -2,4 +2,36 @@ package model;
 
 public class DestinationPort {
 
+	private int ID;
+	private String name;
+	private String unlo;
+	
+	public DestinationPort() {}
+	
+	public DestinationPort(int id, String name, String unlo) {
+		this.ID = id;
+		this.name = name;
+		this.unlo = unlo;
+	}
+	
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUnlo() {
+		return unlo;
+	}
+	public void setUnlo(String unlo) {
+		this.unlo = unlo;
+	}
+	
+	
 }
