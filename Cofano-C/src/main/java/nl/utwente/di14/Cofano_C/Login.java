bb22ab296e520de298a49f1f84bf3db6b16df1ea
Jesse van der Velden <j.vandervelden@student.utwente.nl>
2018-05-26 23:10:54 +0200

    Google login working

---
+++ Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/Login.java
@@ -0,0 +1,119 @@
+package nl.utwente.di14.Cofano_C;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
+/**
+ * Servlet for logging in
+ * @author jesse
+ *
+ */
+
+
+@WebServlet(description="Login Servlet", urlPatterns={"/login"})
+
+public class Login extends HttpServlet {
+	private static final Collection<String> SCOPES = Arrays.asList("email", "profile");
+	private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+	private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+	private GoogleAuthorizationCodeFlow flow;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		String state = new BigInteger(130, new SecureRandom()).toString(32);  // prevent request forgery
+		request.getSession().setAttribute("state", state);
+
+		request.getSession().setAttribute("loginDestination", "/dashboard");
+
+		flow = new GoogleAuthorizationCodeFlow.Builder(
+				HTTP_TRANSPORT,
+				JSON_FACTORY,
+				getServletContext().getInitParameter("google.clientID"),
+		        getServletContext().getInitParameter("google.clientSecret"),
+				SCOPES)
+				.build();
+
+		// Callback url should be the one registered in Google Developers Console
+		GenericUrl callbackUrl = new GenericUrl(request.getRequestURL().toString());
+		callbackUrl.setRawPath(getServletContext().getInitParameter("cofano.url") + "/oauth2callback");
+		String redirectUrl =
+				flow.newAuthorizationUrl()
+						.setRedirectUri(callbackUrl.toString())
+						.setState(state)            // Prevent request forgery
+						.build();
+		response.sendRedirect(redirectUrl);
+	}
+}
+
+//public class Login extends AbstractAuthorizationCodeServlet {
+//
+//
+//
+//	// Set the data that we want to access
+//	private static final Collection<String> SCOPES = Arrays.asList("email", "profile");
+//
+//	  @Override
+//	  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//	      throws IOException, ServletException {
+//		  RequestDispatcher view = request.getRequestDispatcher("index.html");
+//	      view.forward(request, response);
+//	  }
+//
+//	  @Override
+//	  protected String getRedirectUri(HttpServletRequest req) throws ServletException, IOException {
+//	    GenericUrl url = new GenericUrl(req.getRequestURL().toString());
+//	    url.setRawPath(getServletContext().getInitParameter("cofano.url") + "/oauth2callback");
+//	    return url.build();
+//	  }
+//
+//	  /*
+//	  @Override
+//	  protected AuthorizationCodeFlow initializeFlow() throws IOException {
+//	    return new AuthorizationCodeFlow.Builder(BearerToken.authorizationHeaderAccessMethod(),
+//	        new NetHttpTransport(),
+//	        new JacksonFactory(),
+//	        new GenericUrl("https://server.example.com/token"),
+//	        new BasicAuthentication("s6BhdRkqt3", "7Fjfp0ZBr1KtDRbnfVdmIw"),
+//	        "s6BhdRkqt3",
+//	        "https://server.example.com/authorize").setCredentialDataStore(
+//	            StoredCredential.getDefaultDataStore(
+//	                new FileDataStoreFactory(new File("datastoredir"))))
+//	        .build();
+//	  }
+//	  */
+//
+//	  @Override
+//	  protected AuthorizationCodeFlow initializeFlow() throws IOException {
+//	    return new GoogleAuthorizationCodeFlow.Builder(
+//	        new NetHttpTransport(),
+//	        JacksonFactory.getDefaultInstance(),
+//	        getServletContext().getInitParameter("google.clientID"),
+//	        getServletContext().getInitParameter("google.clientSecret"),
+//	        SCOPES)
+//	    	.setApprovalPrompt("auto")
+//	    	.build();
+//	  }
+//
+//	  @Override
+//	  protected String getUserId(HttpServletRequest req) throws ServletException, IOException {
+//	    // return user ID
+//		return null;
+//	  }
+//}
---
+++ Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/LoginCallback.java
@@ -0,0 +1,95 @@
+package nl.utwente.di14.Cofano_C;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.auth.oauth2.*;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.http.*;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
+@WebServlet(description="LoginCallback Servlet", urlPatterns={"/oauth2callback"})
+public class LoginCallback extends HttpServlet {
+	private static final Collection<String> SCOPES = Arrays.asList("email", "profile");
+	private static final String USERINFO_ENDPOINT
+			= "https://www.googleapis.com/plus/v1/people/me/openIdConnect";
+	private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+	private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+	private GoogleAuthorizationCodeFlow flow;
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+		// Ensure that this is no request forgery going on, and that the user
+		// sending us this connect request is the user that was supposed to.
+		if (req.getSession().getAttribute("state") == null
+				|| !req.getParameter("state").equals((String) req.getSession().getAttribute("state"))) {
+			resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			resp.sendRedirect("/");
+			return;
+		}
+
+		req.getSession().removeAttribute("state");     // Remove one-time use state.
+
+		flow = new GoogleAuthorizationCodeFlow.Builder(
+				HTTP_TRANSPORT,
+				JSON_FACTORY,
+				getServletContext().getInitParameter("google.clientID"),
+				getServletContext().getInitParameter("google.clientSecret"),
+				SCOPES)
+				.setApprovalPrompt("auto")
+				.build();
+
+		GenericUrl callbackUrl = new GenericUrl(req.getRequestURL().toString());
+		callbackUrl.setRawPath(getServletContext().getInitParameter("cofano.url") + "/oauth2callback");
+
+		final TokenResponse tokenResponse =
+				flow.newTokenRequest(req.getParameter("code"))
+						.setRedirectUri(callbackUrl.toString())
+						.execute();
+
+		req.getSession().setAttribute("token", tokenResponse.toString()); // Keep track of the token.
+		final Credential credential = flow.createAndStoreCredential(tokenResponse, null);
+		final HttpRequestFactory requestFactory = HTTP_TRANSPORT.createRequestFactory(credential);
+
+		final GenericUrl url = new GenericUrl(USERINFO_ENDPOINT);      // Make an authenticated request.
+		final HttpRequest request = requestFactory.buildGetRequest(url);
+		request.getHeaders().setContentType("application/json");
+
+		final String jsonIdentity = request.execute().parseAsString();
+		HashMap<String, String> userIdResult = null;
+		try {
+			userIdResult = new ObjectMapper().readValue(jsonIdentity, HashMap.class);
+		} catch (JsonMappingException e) {
+			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			resp.setContentType("text/html");
+			PrintWriter out = resp.getWriter();
+			out.append("Oops! Troubles logging in with Google Oauth.");
+			out.close();
+		}
+		// From this map, extract the relevant profile info and store it in the session.
+		// See also: https://developers.google.com/+/web/api/rest/openidconnect/getOpenIdConnect
+		req.getSession().setAttribute("userEmail", userIdResult.get("email"));
+		req.getSession().setAttribute("userId", userIdResult.get("sub"));
+		req.getSession().setAttribute("userImageUrl", userIdResult.get("picture"));
+		req.getSession().setAttribute("userName", userIdResult.get("given_name"));
+		req.getSession().setAttribute("userFamilyName", userIdResult.get("family_name"));
+		resp.sendRedirect(getServletContext().getInitParameter("cofano.url") +
+				req.getSession().getAttribute("loginDestination"));
+	}
+
+}
