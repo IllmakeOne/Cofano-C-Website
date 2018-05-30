922455d4fddc02c425323da8559267b0a2760794
P.H. Knot <p.h.knot@student.utwente.nl>
2018-05-30 11:51:08 +0200

    Updated with new info

--- schema.sql
+++ schema.sql
@@ -1,74 +1,90 @@
 CREATE TABLE ports(
+ id integer NOT NULL AUTO_INCREMENT,
  name varchar NOT NULL,
  unlo varchar,
- p_key integer,
- PRIMARY KEY(p_key)
+ PRIMARY KEY(id)
 );
 
 CREATE TABLE terminals(
+id integer NOT NULL AUTO_INCREMENT,
  name varchar NOT NULL,
- terminal_code pk,
- type varchar NOT NULL,
- unlo varchar NOT NULL,
- PRIMARY KEY(terminal_code)
+ terminal_code varchar NOT NULL,
+ type varchar,
+ unlo varchar,
+ PRIMARY KEY(id)
 );
 
 CREATE TABLE seaships(
- imo varchar,
+id integer NOT NULL AUTO_INCREMENT,
+ imo varchar NOT NULL,
  name varchar NOT NULL,
  callsign varchar,
- mmsi varchar ,
- depth integer,
- s_id integer,
- PRIMARY KEY(s_id)
+ mmsi character(9),
+ ship_depth real,
+ PRIMARY KEY(id)
 );
 
 
 CREATE TABLE undgs(
+ id integer NOT NULL AUTO_INCREMENT,
+ classification varchar NOT NULL,
+ classification_code varchar NOT NULL,
+ undgs_tank_special_provisions_id integer REFERENCES undgs_tank_special_provisions.id,
+ collective boolean,
+ undgs_descriptions_id integer NOT NULL REFERENCES undgs_descriptions.id, 
  hazard_no varchar,
- classification_code varchar,
+ undgs_labels_id integer REFERENCES undgs_labels.id,
+ not_applicable boolean, 
+ packing_group integer, 
  station varchar,
- transport_forbidden boolean,
+ undgs_tankcodes_id integer REFERENCES undgs_tankcodes.id
  transport_category varchar,
- collective boolean,
- un_no integer,
- classification varchar,
- packing_group integer ,
- not_applicable boolean,
- vehicleTank_carriage varchar,
+ transport_forbidden boolean,
  tunnel_code varchar,
- collective boolean,
- undgs_id integer,
- PRIMARY KEY(undgs_id)
+ un_no integer,
+ vehicleTank_carriage varchar,
+ PRIMARY KEY(id)
 );
 
 CREATE TABLE undgs_labels(
-
-)
+name varchar NOT NULL,
+id integer NOT NULL auto_increment,
+undgs_id integer NOT NULL REFERENCES undgs,
+PRIMARY KEY (id)
+);
 
 CREATE TABLE undgs_tank_special_provisions(
-name varchar
-)
+name varchar NOT NULL,
+id integer NOT NULL auto_increment,
+undgs_id integer NOT NULL REFERENCES undgs,
+PRIMARY KEY (id)
+);
 
 CREATE TABLE undgs_tankcodes(
-
+name varchar NOT NULL,
+id integer NOT NULL auto_increment,
+undgs_id integer NOT NULL REFERENCES undgs,
+PRIMARY KEY (id)
 );
 
 CREATE TABLE undgs_descriptions(
-language varchar,
-
+ undgs_language varchar NOT NULL,
+ decription varchar NOT NULL,
+ id integer auto_increment NOT NULL,
+ undgs_id integer NOT NULL REFERENCES undgs,
+ PRIMARY KEY (id)
 );
 
 
-CREATE TABLE contianer_types(
- display_name varchar,
+CREATE TABLE container_types(
+ id integer NOT NULL AUTO_INCREMENT,
+ display_name varchar NOT NULL,
  iso_code varchar,
  description varchar,
- length integer,
- height integer,
+ c_length integer NOT NULL,
+ c_height integer NOT NULL,
  reefer boolean,
- c_type integer,
- PRIMARY KEY(c_type)
+ PRIMARY KEY(id)
 );
 
 
