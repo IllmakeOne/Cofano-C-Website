6ecdfbd5a8f2a601159265eee8732bce13ffcc4d
Roberrt <banu_roby@yahoo.com>
2018-06-01 14:21:00 +0200

    Fixed

--- Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/data/Apps.java
+++ Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/data/Apps.java
@@ -14,7 +14,7 @@ public class Apps extends HttpServlet {
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-	//	request.setAttribute("page", "data/apps");           // Tells base.jsp to include form.jsp
+		request.setAttribute("page", "data/apps");           // Tells base.jsp to include form.jsp
 		request.getRequestDispatcher("/WEB-INF/jsp/data/apps.jsp").forward(request, response);
 	}
 }
