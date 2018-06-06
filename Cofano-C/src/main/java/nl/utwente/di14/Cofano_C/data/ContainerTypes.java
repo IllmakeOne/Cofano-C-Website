a174d21424b6e9f466336ea4b2a6451806de4bfd
s1865870 <p.h.knot@student.utwente.nl>
2018-06-06 13:09:38 +0200

    small fix

--- Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/data/ContainerTypes.java
+++ Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/data/ContainerTypes.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(description="Ship Data Servlet", urlPatterns={"/ships"})
+@WebServlet(description="Ship Data Servlet", urlPatterns={"/containertypes"})
 
 public class ContainerTypes extends HttpServlet {
     @Override
