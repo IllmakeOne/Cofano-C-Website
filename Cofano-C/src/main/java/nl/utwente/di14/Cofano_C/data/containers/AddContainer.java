c9bec7aac53507a4f9162b0993f821823fcc041a
P.H. Knot <p.h.knot@student.utwente.nl>
2018-06-21 12:45:52 +0200

    Fixed checkstyle errors

--- Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/data/containers/AddContainer.java
+++ Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/data/containers/AddContainer.java
@@ -5,18 +5,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import nl.utwente.di14.Cofano_C.util.RequestHelper;
 
 import java.io.IOException;
 
-@WebServlet(description="Application Data Servlet", urlPatterns={"/addcontainer"})
+@WebServlet(description = "Application Data Servlet", urlPatterns = {"/addcontainer"})
 
 public class AddContainer extends HttpServlet {
 
-	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		RequestHelper.show(request, response, getServletContext(), "/WEB-INF/jsp/data/containers/edit.jsp");
-	}
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        RequestHelper.show(request, response, getServletContext(),
+                "/WEB-INF/jsp/data/containers/edit.jsp");
+    }
 }
