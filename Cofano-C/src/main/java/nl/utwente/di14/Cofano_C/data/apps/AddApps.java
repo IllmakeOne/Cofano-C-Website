b1baa58e4b6f4836ef5bc93b640e180ad8459dae
P.H. Knot <p.h.knot@student.utwente.nl>
2018-06-21 12:19:07 +0200

    Fixed checkstyle errors

--- Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/data/apps/AddApps.java
+++ Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/data/apps/AddApps.java
@@ -5,23 +5,23 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import nl.utwente.di14.Cofano_C.util.RequestHelper;
 
 import java.io.IOException;
 
-import static org.apache.commons.lang3.StringEscapeUtils.escapeHtml4;
-
-@WebServlet(description="Application Data Servlet", urlPatterns={"/applications/add"})
+@WebServlet(description = "Application Data Servlet", urlPatterns = {"/applications/add"})
 
 public class AddApps extends HttpServlet {
 
-	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		request.setAttribute("action", "Add");
-		request.setAttribute("method", "post");
-		request.setAttribute("formUrl", getServletContext().getInitParameter("cofano.url") + "/api/applications/add" );
-		RequestHelper.show(request, response, getServletContext(), "/WEB-INF/jsp/data/apps/edit.jsp");
-	}
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        request.setAttribute("action", "Add");
+        request.setAttribute("method", "post");
+        request.setAttribute("formUrl", getServletContext().getInitParameter("cofano.url") +
+                "/api/applications/add");
+        RequestHelper.show(request, response, getServletContext(),
+                "/WEB-INF/jsp/data/apps/edit.jsp");
+    }
 }
