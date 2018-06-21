d1bff9c31f9951f98a742f00b7cd193e1ab6d6bd
P.H. Knot <p.h.knot@student.utwente.nl>
2018-06-21 12:49:01 +0200

    Added javadoc

--- Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/data/apps/Apps.java
+++ Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/data/apps/Apps.java
@@ -9,10 +9,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * Data object for apps.
+ */
 @WebServlet(description = "Application Data Servlet", urlPatterns = {"/applications"})
-
 public class Apps extends HttpServlet {
 
+    /**
+     * Handles the GET request.
+     *
+     * @param request  the HTTP request
+     * @param response the HTTP response
+     * @throws IOException      when there is an <code>IOException</code>
+     * @throws ServletException when there is a <code>ServletException</code>
+     */
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
