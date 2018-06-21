e4db29d5a54795bc8c0a81cd2f7bf51b09f0e5fe
P.H. Knot <p.h.knot@student.utwente.nl>
2018-06-21 12:22:41 +0200

    Fixed checkstyle errors

--- Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/data/apps/Apps.java
+++ Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/data/apps/Apps.java
@@ -7,16 +7,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(description="Application Data Servlet", urlPatterns={"/applications"})
+@WebServlet(description = "Application Data Servlet", urlPatterns = {"/applications"})
 
 public class Apps extends HttpServlet {
 
-	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		RequestHelper.show(request, response, getServletContext(), "/WEB-INF/jsp/data/apps/overview.jsp");
-		//Sprint 4 :)
-	}
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        RequestHelper.show(request, response, getServletContext(),
+                "/WEB-INF/jsp/data/apps/overview.jsp");
+        //Sprint 4 :)
+    }
 }
