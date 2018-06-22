5b6e4b93b6f6ce48142eca8627955e55cad0ebae
P.H. Knot <p.h.knot@student.utwente.nl>
2018-06-22 10:04:13 +0200

    Cleaned up code

--- Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/exceptions/ExceptionInfo.java
+++ Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/exceptions/ExceptionInfo.java
@@ -5,15 +5,27 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class ExceptionInfo {
-	private int status;
-	private String msg, desc;
-	public ExceptionInfo(int status, String msg, String desc) {
-		this.status = status;
-		this.msg = msg;
-		this.desc = desc;
-	}
+    private final int status;
+    private final String msg, desc;
 
-	@XmlElement public int getStatus() { return status; }
-	@XmlElement public String getMessage() { return msg; }
-	@XmlElement public String getDescription() { return desc; }
+    public ExceptionInfo(int status, String msg, String desc) {
+        this.status = status;
+        this.msg = msg;
+        this.desc = desc;
+    }
+
+    @XmlElement
+    public int getStatus() {
+        return status;
+    }
+
+    @XmlElement
+    public String getMessage() {
+        return msg;
+    }
+
+    @XmlElement
+    public String getDescription() {
+        return desc;
+    }
 }
