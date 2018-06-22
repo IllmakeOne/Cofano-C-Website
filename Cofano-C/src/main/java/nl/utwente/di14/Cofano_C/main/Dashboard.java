7ac13fe3b6e53d756943ee8aa78efae0f0194159
P.H. Knot <p.h.knot@student.utwente.nl>
2018-06-22 10:12:13 +0200

    Removed commented code

--- Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/main/Dashboard.java
+++ Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/main/Dashboard.java
@@ -7,38 +7,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(description="Dashboard Servlet", urlPatterns={"/dashboard"})
-
+@WebServlet(description = "Dashboard Servlet", urlPatterns = {"/dashboard"})
 public class Dashboard extends HttpServlet {
 
-	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		if (req.getSession().getAttribute("token") != null) {
-			req.getRequestDispatcher("index.jsp").forward(req, resp);
-		} else {
-			resp.sendRedirect(getServletContext().getInitParameter("cofano.url") + "/login");
-		}
-	}
-
-//	@Override
-//	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//		response.setContentType("text/html");
-//		PrintWriter out = response.getWriter();
-//		String docType =
-//				"<!DOCTYPE HTML>\n";
-//		String title = "Dashboard coming soon...";
-//		out.println(docType +
-//				"<HTML>\n" +
-//				"<HEAD><TITLE>" + title + "</TITLE>" +
-//				"</HEAD>\n" +
-//				"<BODY BGCOLOR=\"#FDF5E6\">\n" +
-//				"<H1>" + title + "</H1>\n" +
-//
-//				" <P>HOERAAAAA: " + request.getSession().getAttribute("userEmail") + "</P>\n" +
-//				" <P>HOERAAAAA: " + request.getSession().getAttribute("userName") + "</P>\n" +
-//				" <P>HOERAAAAA: " + request.getSession().getAttribute("userFamilyName") + "</P>\n" +
-//				" <a href='./logout'>Loguit</a>\n" +
-//
-//				"</BODY></HTML>");
-//	}
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getSession().getAttribute("token") != null) {
+            req.getRequestDispatcher("index.jsp").forward(req, resp);
+        } else {
+            resp.sendRedirect(getServletContext().getInitParameter("cofano.url") + "/login");
+        }
+    }
 }
