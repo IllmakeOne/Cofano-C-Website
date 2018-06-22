b59a69759e7e747ec058ee0d8b946dff4302917c
P.H. Knot <p.h.knot@student.utwente.nl>
2018-06-22 10:16:59 +0200

    Added Javadoc and fixed Checkstyle errors

--- Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/main/Dashboard.java
+++ Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/main/Dashboard.java
@@ -7,11 +7,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * This is the data object class for the 'Dashboard' web servlet.
+ */
 @WebServlet(description = "Dashboard Servlet", urlPatterns = {"/dashboard"})
 public class Dashboard extends HttpServlet {
 
+    /**
+     * This method handles the <code>HttpServletRequest</code>s.
+     *
+     * @param req  the HTTP Servlet request
+     * @param resp the HTTP Servlet response
+     * @throws ServletException when there is a servlet exception
+     * @throws IOException      when there is an IO exception
+     */
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         if (req.getSession().getAttribute("token") != null) {
             req.getRequestDispatcher("index.jsp").forward(req, resp);
         } else {
