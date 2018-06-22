7929749146ec177ee752bdb43b3dacd98997ab70
P.H. Knot <p.h.knot@student.utwente.nl>
2018-06-22 12:00:02 +0200

    Added Javadoc and fixed checkstyle errors

--- Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/model/Port.java
+++ Cofano-C/src/main/java/nl/utwente/di14/Cofano_C/model/Port.java
@@ -9,43 +9,86 @@ public class Port {
     private String name;
     private String unlo;
 
+    /**
+     * Constructs a port without data.
+     */
     public Port() {
     }
 
+    /**
+     * Constructs a port with data.
+     *
+     * @param id   of the port
+     * @param name of the port
+     * @param unlo of the port
+     */
     public Port(int id, String name, String unlo) {
         this.id = id;
         this.name = name;
         this.unlo = unlo;
     }
 
+    /**
+     * Converts the information of the port to a string.
+     *
+     * @return the string with info about this port object
+     */
     @Override
     public String toString() {
         return "Port:  Name: " + name + "; Unlo: " + unlo;
     }
 
+    /**
+     * Gets the ID of the port.
+     *
+     * @return ID of the port
+     */
     public int getId() {
         return id;
     }
 
-    public void setId(int iD) {
-        id = iD;
+    /**
+     * Sets the ID of the port.
+     *
+     * @param id of the port
+     */
+    public void setId(int id) {
+        this.id = id;
     }
 
+    /**
+     * Gets the name of the port.
+     *
+     * @return the name of the port
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the port.
+     *
+     * @param name of the port
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the UNLO of the port.
+     *
+     * @return the UNLO of the port
+     */
     public String getUnlo() {
         return unlo;
     }
 
+    /**
+     * Sets the UNLO of the port.
+     *
+     * @param unlo of the port
+     */
     public void setUnlo(String unlo) {
         this.unlo = unlo;
     }
-
-
 }
