b7546b1a5f23bcafd51f730ae11dc1b5c5def794
P.H. Knot <p.h.knot@student.utwente.nl>
2018-06-27 11:43:44 +0200

    cleanup

--- Cofano-C/src/test/selenium/loginTest.java
+++ Cofano-C/src/test/selenium/loginTest.java
@@ -1,19 +1,11 @@
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.xml.sax.Locator;
 
 
 public class loginTest {
