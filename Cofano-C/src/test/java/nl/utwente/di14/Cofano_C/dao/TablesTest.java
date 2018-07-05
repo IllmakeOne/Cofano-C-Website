792a06a8b086bbf1e2a787a99dbae473c0b22ca8
P.H. Knot <p.h.knot@student.utwente.nl>
2018-07-05 12:17:19 +0200

    More work on the test

--- Cofano-C/src/test/java/nl/utwente/di14/Cofano_C/dao/TablesTest.java
+++ Cofano-C/src/test/java/nl/utwente/di14/Cofano_C/dao/TablesTest.java
@@ -1,17 +1,24 @@
 package nl.utwente.di14.Cofano_C.dao;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.SQLException;
+
 import static nl.utwente.di14.Cofano_C.dao.Tables.getInstance;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class TablesTest {
 
+    Tables tables = new Tables();
+
+    BasicDataSource basicDataSource = new BasicDataSource();
+
     @Before
     public void setup() {
-        Tables tables = new Tables();
     }
 
     @Test
@@ -21,19 +28,34 @@ public class TablesTest {
 
     @Test
     public void getBds() {
+        assertNotNull(tables.getBds());
     }
 
     @Test
     public void setBds() {
+        tables.setBds(basicDataSource);
+        assertEquals(basicDataSource, tables.getBds());
     }
 
     @Test
     public void getCon() {
+        try {
+            assertNotNull(Tables.getCon());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void addtoConflicts() {
+        try {
+            Tables.addtoConflicts(Tables.getCon(), "ship", "JUnit", 909090, 808080);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        String query = "SELECT * FROM  conflict WHERE culprit = "JUnit"";
     }
+
 
     @Test
     public void objToPGobj() {
