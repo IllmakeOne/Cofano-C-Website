20e36cd3aab15a6e5bf65d00f6085db60cb47dd8
P.H. Knot <p.h.knot@student.utwente.nl>
2018-07-05 10:09:31 +0200

    Added test class

---
+++ Cofano-C/src/test/java/nl/utwente/di14/Cofano_C/resources/ApplicationsResourceTest.java
@@ -0,0 +1,38 @@
+package nl.utwente.di14.Cofano_C.resources;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplicationsResourceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getAllApps() {
+    }
+
+    @Test
+    public void generateAPI() {
+    }
+
+    @Test
+    public void getApp() {
+    }
+
+    @Test
+    public void deleteApp() {
+    }
+
+    @Test
+    public void updateApp() {
+    }
+}
